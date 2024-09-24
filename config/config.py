9d7e0fab1159fb5fd57180a9e4fb74ee6926d9d5
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-24 16:14:57 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -95,7 +95,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RishuxCoder/VIP-MUSIC",
+    "https://github.com/Rishabhopi/MY-VIP-MUSIC-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
@@ -108,14 +108,14 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/NenoBots"
+    "SUPPORT_CHANNEL", "https://t.me/ur_rishu_143"
 )  # Example:- https://t.me/VIP_CREATORS
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/FRIENDSCHATZONEOFFCIAL"
+    "SUPPORT_GROUP", "https://t.me/ur_support07"
 )  # Example:- https://t.me/TG_FRIENDSS
 
 SUPPORT_CHAT = getenv(
-    "SUPPORT_GROUP", "https://t.me/FRIENDSCHATZONEOFFCIAL"
+    "SUPPORT_GROUP", "https://t.me/ur_support07"
 )  # Example:- https://t.me/TG_FRIENDSS
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
@@ -138,7 +138,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/RishuxCoder/VIP-MUSIC")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/Rishabhopi/MY-VIP-MUSIC-")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
